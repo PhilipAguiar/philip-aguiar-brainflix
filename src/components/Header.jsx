3aac745e542ce8151e8283e5d34625c8494b8a33
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-23 01:55:30 -0500

    added base mixins to appropriate scss classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,10 @@ function Header() {
   return (
     <section className="header">
       <img className="header__logo" src={logo} alt="" />
-      <input className="header__input" type="text" />
-      <img className="header__avatar" src={avatar} alt="" />
+      <div className="header__search">
+        <input className="header__input" type="text" />
+        <img className="header__avatar" src={avatar} alt="" />
+      </div>
       <button className="header__button">UPLOAD</button>
     </section>
   );
