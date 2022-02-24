3242d6ccd7d22eb190fb38a08b4b73e9d0898dac
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-24 18:13:22 -0500

    added key to array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import videoListJSON from "./data/video-details.json";
 import Header from "./components/Header/Header";
-import HeroPlayer from "./components/HeroPlayer/HeroPlayer";
 import React from "react";
 import ActiveVideo from "./components/ActiveVideo/ActiveVideo";
 
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,8 +4,8 @@ import "./CommentList.scss";
 function CommentList(props) {
     
   return <section className="comments">
-      {props.comments.map(comment => {
-       return <CommentItem comment = {comment}/>
+      {props.comments.map((comment,index) => {
+       return <CommentItem key = {index} comment = {comment}/>
       })}
   </section>;
 }
