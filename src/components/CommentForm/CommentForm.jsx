1633bce7f50741cc9672033059ada120b199e7c3
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-24 10:42:55 -0500

    changed class names for commentform

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -2,16 +2,16 @@ import "./CommentForm.scss";
 
 function CommentForm(props) {
   return (
-    <section className="comments">
-      <h5 className="comments__count">{props.video.comments.length} Comments</h5>
-      <div className="comments__wrapper">
-        <div className="comments__image-container">
-          <div className="comments__image"></div>
+    <section className="post">
+      <h5 className="post__count">{props.video.comments.length} Comments</h5>
+      <div className="post__wrapper">
+        <div className="post__image-container">
+          <div className="post__image"></div>
         </div>
-        <form className="comments__form">
-          <label className="comments__label">Join the Conversation</label>
-          <textarea name="comment" className="comments__input comments__input--large" placeholder="Add a new comment"></textarea>
-          <button className="comments__button">COMMENT</button>
+        <form className="post__form">
+          <label className="post__label">Join the Conversation</label>
+          <textarea name="comment" className="post__input" placeholder="Add a new comment"></textarea>
+          <button className="post__button">COMMENT</button>
         </form>
       </div>
     </section>
