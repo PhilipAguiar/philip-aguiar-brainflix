70b60f553e057950519f4d568ba9a656718b13ac
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-24 20:04:26 -0500

    created comment List component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-import videoListJSON from "./data/video-details.json";
+import videoListDetailed from "./data/video-details.json";
+import videoListSimple from "./data/videos.json";
 import Header from "./components/Header/Header";
 import React from "react";
 import ActiveVideo from "./components/ActiveVideo/ActiveVideo";
+import VideoList from "./components/VideoList/VideoList";
 
 class App extends React.Component {
   state = {
-    videoList: videoListJSON,
+    videoList: videoListDetailed,
+    simpleVideoList:  videoListSimple
   };
 
   render() {
@@ -14,6 +17,7 @@ class App extends React.Component {
       <>
         <Header />
         <ActiveVideo video={this.state.videoList[0]} />
+        <VideoList videoList={videoListSimple}/>
       </>
     );
   }
---
+++ src/components/VideoList/VideoList.jsx
@@ -0,0 +1,19 @@
+import "./VideoList.scss";
+
+function VideoList({ videoList }) {
+  console.log(videoList)
+  return <section>
+      
+      <h5>NEXT VIDEOS</h5>
+      
+      {videoList.map((video) => {
+
+        return {
+            
+        }
+
+
+  })}</section>;
+}
+
+export default VideoList;
