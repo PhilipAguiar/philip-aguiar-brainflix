873642ab99d1880c30fcffcaa8238805a9810241
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-26 11:18:03 -0500

    improved comment component styling for mobile

--- src/components/CommentItem/CommentItem.jsx
+++ src/components/CommentItem/CommentItem.jsx
@@ -1,15 +1,21 @@
-import "./CommentItem.scss"
+import "./CommentItem.scss";
 
-function CommentItem({comment}) {
-    console.log(comment)
+function CommentItem({ comment }) {
+  console.log(comment);
   return (
-    <div>
-        <p>{comment.name}</p>
-        <p>{comment.comment}</p>
-        <p>{comment.likes}</p>
-        <p>{comment.timestamp}</p>
+    <div className="comment">
+      <div className="comment__image-container">
+        <div className="comment__image"></div>
+      </div>
+      <div className="comment__info-container">
+        <div className="comment__user-wrapper">
+          <p className="comment__name">{comment.name}</p>
+          <p className="comment__date">{comment.timestamp}</p>
+        </div>
+        <p className="comment__text">{comment.comment}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CommentItem
+export default CommentItem;
