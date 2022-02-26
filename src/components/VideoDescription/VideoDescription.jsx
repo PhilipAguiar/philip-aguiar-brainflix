f7896fd2eca7eddc9ea687531ccc9f7da8d2e5a0
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-26 17:20:04 -0500

    updated active video title section for tablet

--- src/components/VideoDescription/VideoDescription.jsx
+++ src/components/VideoDescription/VideoDescription.jsx
@@ -10,13 +10,13 @@ function VideoDescription({title, timestamp, channel, views, likes, description}
       <h1 className="description__title">{title}</h1>
       <div className="description__container">
         <div className="description__info-wrapper">
-          <h4 className="description__stat description__stat--bold">By {channel}</h4>
+          <h4 className="description__stat description__stat--bold description__stat--tablet">By {channel}</h4>
           <p className="description__stat description__stat--bottom">{date.toLocaleDateString("en-US")}</p>
         </div>
         <div className="description__info-wrapper description__info-wrapper--right">
           <div className="description__stats-wrapper">
             <img className="description__icon" src={viewsIcon} alt="eye" />
-            <p className="description__stat">{views}</p>
+            <p className="description__stat description__stat--tablet">{views}</p>
           </div>
           <div className="description__stats-wrapper description__stats-wrapper--bottom">
             <img className="description__icon" src={likeIcon} alt="heart" />
