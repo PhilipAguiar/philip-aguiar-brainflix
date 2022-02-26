058a4b59cfd3cce05836b48bfa8426ccfaba1fcf
Philip Aguiar <PhilipAguiar@gmail.com>
2022-02-26 13:27:54 -0500

    next video section resembles mockup more closely

--- src/components/VideoItem/VideoItem.jsx
+++ src/components/VideoItem/VideoItem.jsx
@@ -1,13 +1,17 @@
-import './VideoItem.scss'
+import "./VideoItem.scss";
 
-function VideoItem({video , clickedVideo}) {
+function VideoItem({ video, clickedVideo }) {
   return (
-    <div id={video.id} onClick={clickedVideo}>
-        <img src={video.image} alt="" />
-        <h3>{video.title}</h3>
-        <p>{video.channel}</p>
+    <div className="videoItem" id={video.id} onClick={clickedVideo}>
+      <div className="videoItem__image-wrapper">
+        <img className="videoItem__image" src={video.image} alt="" />
+      </div>
+      <div className="videoItem__text-wrapper">
+        <h3 className="videoItem__title">{video.title}</h3>
+        <p className="videoItem__channel">{video.channel}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default VideoItem
+export default VideoItem;
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -2,10 +2,9 @@ import VideoItem from "../VideoItem/VideoItem";
 import "./VideoList.scss";
 
 function VideoList({ videoList, clickedVideo }) {
-  console.log(videoList);
   return (
-    <section>
-      <h5>NEXT VIDEOS</h5>
+    <section className="next-videos">
+      <h5 className="next-videos__title">NEXT VIDEOS</h5>
 
       {videoList.map((video) => {
         return <VideoItem video = {video} clickedVideo = {clickedVideo}/>;
