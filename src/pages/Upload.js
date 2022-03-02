dd11a74eeaf3b1bb6bca03108821e97e4e6a36fc
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-01 20:41:09 -0500

    created upload page elements (no styling)

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import uploadImage from '../assets/Images/Upload-video-preview.jpg'
 
 function Upload() {
   return (
-    <div>Upload</div>
-  )
+    <section>
+      <h1>UPLOAD VIDEO</h1>
+      <p>VIDEO THUMBNAIL</p>
+      <img src={uploadImage}></img>
+      <form>
+        <label>TITLE YOUR VIDEO</label>
+        <input></input>
+        <label>TITLE YOUR VIDEO</label>
+        <textarea></textarea>
+        <button></button>
+
+        <h4>Cancel</h4>
+
+      </form>
+    </section>
+  );
 }
 
-export default Upload
+export default Upload;
