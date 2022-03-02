3a2aff9493f10695714ae7983c6adc9b3aed8fb8
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-01 19:47:34 -0500

    added browser router with links to home and upload pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import "./App.scss";
-import videoListDetailed from "./data/video-details.json";
-import videoListSimple from "./data/videos.json";
 import Header from "./components/Header/Header";
-import HeroPlayer from "./components/HeroPlayer/HeroPlayer";
 import React from "react";
-import ActiveVideo from "./components/ActiveVideo/ActiveVideo";
-import VideoList from "./components/VideoList/VideoList";
-import timestampConverter from "./modules/TimeStamp";
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Upload from "./pages/Upload";
 
 class App extends React.Component {
-  
-
   render() {
-    
     return (
-      <>
-      <Header/>
-        <Home />
-      </>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+        <Route path="/" exact render={() => <Home />} />
+        <Route path="/upload" render={() => <Upload />} />
+          <Home />
+          <Upload />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
