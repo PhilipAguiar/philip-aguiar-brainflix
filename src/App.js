9d509b42437be7f469eda57e5bb54624e56a6ee7
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-06 14:31:10 -0500

    refactored app and home page so all state is in home.jsx

--- src/App.js
+++ src/App.js
@@ -4,87 +4,17 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Upload from "./pages/Upload/Upload";
 import Home from "./pages/Home/Home";
-import apiUtils from "./utils/api";
-import timestampConverter from "./utils/TimeStamp";
 
 class App extends React.Component {
-  state = {
-    videoList: null,
-    activeVideo: null,
-  };
-
-  componentDidMount() {
-    apiUtils.getAll().then((response) => {
-      this.setState({
-        videoList: response.data,
-      });
-
-      this.setActiveVideo(response.data[0].id);
-    });
-  }
-
-  setActiveVideo = (activeId) => {
-    if (activeId) {
-      apiUtils.getVideoById(activeId).then((response) => {
-        this.setState({
-          activeVideo: response.data,
-        });
-      });
-    } else {
-      this.setDefaultVideo();
-    }
-  };
-
-  setDefaultVideo = () => {
-    apiUtils.getVideoById(this.state.videoList[0].id).then((response) => {
-      this.setState({
-        activeVideo: response.data,
-      });
-    });
-  };
-
   render() {
-    if (!this.state.videoList || !this.state.activeVideo) {
-      return <p>Loading...</p>;
-    }
-
-    const filteredVideoList = this.state.videoList.filter((video) => {
-      return video.id !== this.state.activeVideo.id;
-    });
-
     return (
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route path="/" exact render={(routerProps) => (
-              <Home
-                routerProps={routerProps}
-                timestampConverter={timestampConverter}
-                activeVideo={this.state.activeVideo}
-                setActiveVideo={this.setActiveVideo}
-                videoList={filteredVideoList}
-                clickHandler={this.setActiveVideo}
-              />
-            )}
-          />
-
-          <Route path="/upload" render={(routerProps) => <Upload routerProps={routerProps}/>} />
-          <Route
-            path="/video/:id"
-            render={(routerProps) => (
-              <Home
-                routerProps={routerProps}
-                timestampConverter={timestampConverter}
-                activeVideo={this.state.activeVideo}
-                setActiveVideo={this.setActiveVideo}
-                videoList={filteredVideoList}
-                clickHandler={this.setActiveVideo}
-              />
-            )}
-          />
+          <Route path="/" exact render={(routerProps) => <Home routerProps={routerProps} />} />
+          <Route path="/upload" render={(routerProps) => <Upload routerProps={routerProps} />} />
+          <Route path="/video/:id" render={(routerProps) => <Home routerProps={routerProps} />} />
         </Switch>
-
-        
       </BrowserRouter>
     );
   }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,31 +5,77 @@ import CommentList from "../../components/CommentList/CommentList";
 import VideoDescription from "../../components/VideoDescription/VideoDescription";
 import HeroPlayer from "../../components/HeroPlayer/HeroPlayer";
 import VideoList from "../../components/VideoList/VideoList";
+import apiUtils from "../../utils/api";
+import timestampConverter from "../../utils/TimeStamp";
 
 class ActiveVideo extends Component {
+  state = {
+    videoList: null,
+    activeVideo: null,
+  };
+
+  componentDidMount() {
+    apiUtils.getAll().then((response) => {
+      this.setState({
+        videoList: response.data,
+      });
+      if (this.props.routerProps.match.params.id) {
+        this.setActiveVideo(this.props.routerProps.match.params.id);
+      } else {
+        this.setActiveVideo(response.data[0].id);
+      }
+    });
+  }
+
+  setActiveVideo = (activeId) => {
+    if (activeId) {
+      apiUtils.getVideoById(activeId).then((response) => {
+        this.setState({
+          activeVideo: response.data,
+        });
+      });
+    } else {
+      this.setDefaultVideo();
+    }
+  };
+
+  setDefaultVideo = () => {
+    apiUtils.getVideoById(this.state.videoList[0].id).then((response) => {
+      this.setState({
+        activeVideo: response.data,
+      });
+    });
+  };
 
   componentDidUpdate(prevProps) {
+    console.log(prevProps);
     const videoId = this.props.routerProps.match.params.id;
 
-    if (prevProps.activeVideo.id !== videoId) {
-      this.props.setActiveVideo(this.props.routerProps.match.params.id);
+    if (prevProps.routerProps.match.params.id !== videoId) {
+      this.setActiveVideo(this.props.routerProps.match.params.id);
     }
-
   }
 
   render() {
-    const { activeVideo, timestampConverter, videoList, clickHandler } = this.props;
+    if (!this.state.videoList || !this.state.activeVideo) {
+      return <p>Loading...</p>;
+    }
+
+    const filteredVideoList = this.state.videoList.filter((video) => {
+      return video.id !== this.state.activeVideo.id;
+    });
+
     return (
       <>
-        <HeroPlayer image={activeVideo.image} />
+        <HeroPlayer image={this.state.activeVideo.image} />
 
         <section className="activeVideo">
-          <VideoDescription video={activeVideo} timestampConverter={timestampConverter} />
-          <CommentForm comments={activeVideo.comments} />
-          <CommentList comments={activeVideo.comments} timestampConverter={timestampConverter} />
+          <VideoDescription video={this.state.activeVideo} timestampConverter={timestampConverter} />
+          <CommentForm comments={this.state.activeVideo.comments} />
+          <CommentList comments={this.state.activeVideo.comments} timestampConverter={timestampConverter} />
         </section>
-        
-        <VideoList videoList={videoList} clickHandler={clickHandler} />
+
+        <VideoList videoList={filteredVideoList} clickHandler={this.setActiveVideo} />
       </>
     );
   }
